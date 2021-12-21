2b803ba983d3b1d878f5705ed47209fc82ee1681
Whose dream is this? <whosedreamisthis@gmail.com>
2021-12-21 10:32:45 -0800

    code cleanup

---
+++ controllers/profile.js
@@ -0,0 +1,22 @@
+const handleProfileGet = (db) => (req,res) => { 
+    const {id} = req.params;
+
+    db.select('*').from('users').where({id}).then(user =>{
+        if (user.length) {
+            res.json(user[0]);
+        }
+        else {
+            res.status(400).json('not found.')
+        }
+    }).catch(err => res.status(400).json('error getting user.'))
+    // if (!found) {
+    //     res.status(400).json("not found");
+    // }
+
+    return 
+
+}
+
+module.exports = {
+    handleProfileGet: handleProfileGet
+};
--- server.js
+++ server.js
@@ -1,100 +1,53 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const knex = require ('knex');
+
+const { sumBy } = require('lodash');
+const Clarifai = require( 'clarifai');
+
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+
+const { preProcessFile } = require('typescript');
+//import dotenv from  'dotenv';y
+  const appClarifai = new Clarifai.App({
+    apiKey: '6db181cd35e74f609d3956b56cb11f6f'
+   });
+/**
+ * @type {Knex}
+ */
+ const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'smart-brain'
+    }
+  });
+db.select('*').from('users').then(data => {
+    //console.log(data);
+})
+
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors())
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email:'john@gmail.com',
-            password:'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email:'sally@gmail.com',
-            password:'milk',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id:'987',
-            hash:'',
-            email: 'john@gmail.com'
-        }
-    ]
-}
+app.use(cors());
+
 app.get('/',(req,res) => {
-    res.send(database.users);
+   // res.send(db(.users));
 })
 
-app.post('/signin',(req,res) => {
-    if (req.body.email === database.users[0].email &&
-         req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-         }
-         else {
-             res.status(400).json("Error logging in. ", req);
-         }
-});
+app.post('/signin', signin.handleSignin(db,bcrypt));
 
-app.post('/register',(req,res) => {
-    const {email,name,password} = req.body;
+app.post('/register', register.handleRegister(db,bcrypt));
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-    console.log(hash)
+app.get('/profile/:id', profile.handleProfileGet(db));
 
-
-    database.users.push ( {id: '125',
-    name: name,
-    email:email,
-    entries: 0,
-    joined: new Date()});
-    res.json(database.users[database.users.length - 1])
-});
-
-app.get('/profile/:id', (req,res) => {
-    const {id} = req.params;
-
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    });
-    if (!found) {
-        res.status(400).json("not found");
-    }
-
-    return 
-
-});
-
-app.put('/image', (req,res) => {
-    const {id} = req.body;
-
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-    if (!found) {
-        res.status(400).json("not found");
-    }
-});
+app.put('/image',image.handleImage(db));
 
 
 
