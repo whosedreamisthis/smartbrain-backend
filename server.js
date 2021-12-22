fd15d1a11c22c7efac035b41c7144b5f5331765f
Whose dream is this? <whosedreamisthis@gmail.com>
2021-12-22 11:46:32 -0800

    heroku db

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const image = require('./controllers/image');
  const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-pointy-32515',
       user : 'postgres',
       password : 'test',
       database : 'smart-brain'
